2a3718e6c0068f8c1d0e964f342849b5cafad7f8
DixonKH <khaydarovdilshod9@gmail.com>
2024-05-16 18:04:07 +0900

    fix: modify popular and new products components

--- src/app/screens/homePage/NewProducts.tsx
+++ src/app/screens/homePage/NewProducts.tsx
@@ -4,7 +4,7 @@ import Typography from "@mui/joy/Typography";
 import VisibilityIcon from "@mui/icons-material/Visibility";
 import { AspectRatio, Card, CardOverflow, CssVarsProvider } from "@mui/joy";
 
-const listProducts = [
+const newProducts = [
   { productName: "Mobile", imagePath: "/img/perspiciatis-unde.jpg" },
   { productName: "Sports", imagePath: "/img/watch2.jpg" },
   { productName: "Jewellery", imagePath: "/img/watch3.jpg" },
@@ -19,39 +19,43 @@ export function NewProducts() {
           <Box className="category-title">New Products</Box>
           <Stack className="cards-frame">
             <CssVarsProvider>
-              {listProducts.map((ele, index) => {
-                return (
-                  <Card key={index} variant="outlined" className="card">
-                    <CardOverflow>
-                      <div className="product-sale">New</div>
-                      <AspectRatio ratio="1">
-                        <img src={ele.imagePath} alt="" />
-                      </AspectRatio>
-                    </CardOverflow>
-                    <CardOverflow variant="soft" className="product-detail">
-                      <Stack className="info">
-                        <Stack flexDirection={"row"}>
-                          <Typography className="title">
-                            {ele.productName}
-                          </Typography>
-                          <Typography className="price">$12</Typography>
+              {newProducts.length !== 0 ? (
+                newProducts.map((ele, index) => {
+                  return (
+                    <Card key={index} variant="outlined" className="card">
+                      <CardOverflow>
+                        <div className="product-sale">New</div>
+                        <AspectRatio ratio="1">
+                          <img src={ele.imagePath} alt="" />
+                        </AspectRatio>
+                      </CardOverflow>
+                      <CardOverflow variant="soft" className="product-detail">
+                        <Stack className="info">
+                          <Stack flexDirection={"row"}>
+                            <Typography className="title">
+                              {ele.productName}
+                            </Typography>
+                            <Typography className="price">$12</Typography>
+                          </Stack>
+                          <Stack>
+                            <Typography className="views">
+                              20
+                              <VisibilityIcon
+                                sx={{
+                                  fontSize: 20,
+                                  marginLeft: "5px",
+                                }}
+                              />
+                            </Typography>
+                          </Stack>
                         </Stack>
-                        <Stack>
-                          <Typography className="views">
-                            20
-                            <VisibilityIcon
-                              sx={{
-                                fontSize: 20,
-                                marginLeft: "5px",
-                              }}
-                            />
-                          </Typography>
-                        </Stack>
-                      </Stack>
-                    </CardOverflow>
-                  </Card>
-                );
-              })}
+                      </CardOverflow>
+                    </Card>
+                  );
+                })
+              ) : (
+                <Box className="no-data">New products are not available</Box>
+              )}
             </CssVarsProvider>
           </Stack>
         </Stack>
