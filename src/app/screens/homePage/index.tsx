1564d194b3845683ab11a73ae49f72689f50808e
DixonKH <khaydarovdilshod9@gmail.com>
2024-06-02 23:07:37 +0900

    feat: develop slice and selector usage

--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -6,13 +6,29 @@ import { Testimonal } from "./Testimonal";
 import { Advertisement } from "./Advertisement";
 import "../../../css/home.css";
 
-export function HomePage() {
-  // selector: Data <= Store
+import { useDispatch, useSelector } from "react-redux";
+import { Dispatch } from "@reduxjs/toolkit";
+import { createSelector } from "reselect";
+import { setPopularWatches } from "./slice";
+import { retrivePopularWatches } from "./selector";
+import { Product } from "../../../lib/types/product";
 
-  useEffect(() => {
-    // Backend server data request  => Data
-    // Slice: Data => Store
-  }, []);
+/** REDUX SLICE & SELECTOR */
+const actionDispatch = (dispatch: Dispatch) => ({
+  setPopularWatches: (data: Product[]) => dispatch(setPopularWatches(data)),
+});
+
+const popularWatchesRetriver = createSelector(
+  retrivePopularWatches,
+  (popularWatches) => ({ popularWatches })
+);
+
+export function HomePage() {
+  const { setPopularWatches } = actionDispatch(useDispatch());
+  const { popularWatches } = useSelector(popularWatchesRetriver);
+
+  useEffect(() => {}, []);
+
   return (
     <div className="homepage">
       <BrandDetail />
